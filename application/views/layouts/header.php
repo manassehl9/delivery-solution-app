ecb1c3429319425ec84cb49bb9baef9c9138fa0f
Manasseh Abiodun <manassehl9@gmail.com>
2018-07-13 10:35:19 +0100

    header

--- application/views/layouts/header.php
+++ application/views/layouts/header.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
   <head>
-     <!-- Global site tag (gtag.js) - Google Analytics -->
+    <!-- Global site tag (gtag.js) - Google Analytics -->
     <script async src="https://www.googletagmanager.com/gtag/js?id=UA-120827714-1"></script>
     <script>
       window.dataLayer = window.dataLayer || [];
@@ -10,7 +10,6 @@
 
       gtag('config', 'UA-120827714-1');
     </script>
-
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -23,11 +22,14 @@
     <link href="/saddleng/font-awesome/css/font-awesome.min.css" rel="stylesheet">
     <link rel="stylesheet" href="/saddleng/css/form-elements.css">
     <link rel="stylesheet" href="/saddleng/css/style1.css">
+
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css?family=Encode+Sans+Condensed" rel="stylesheet">
     <link href='https://fonts.googleapis.com/css?family=Lato' rel='stylesheet' type='text/css'>
 
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-120857321-1"></script>
 
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
     <script src="/saddleng/js/scripts.js"></script>
 
@@ -35,7 +37,7 @@
       var i = 0;
 	    function insertItem(elementId) {
 		    ++i;
-		    $('<div style="border-top: 1px solid lightgray;" id="dynamic_field'+i+'" id="row'+i+'"><br><br><br><div class="form-group col-md-6 col-xs-6"  id="item_name'+i+'"><label class="control-label">Item Name:</label><input type="text" name="item_name[]" placeholder="Item Name" class="item_namae form-control" id="item_name"></div><div class="form-group col-md-6 col-xs-6"  id="item_quantity'+i+'"><label class="control-label">Item Quantity:</label><input type="number" name="item_quantity[]" placeholder="Item Quantity" class="item_quantity form-control" id="item_quantity"></div><div class="form-group col-md-6 col-xs-6" id="item_weight'+i+'"><label class="control-label">Item Weight(KG):</label><input type="number" name="item_weight[]" placeholder="Item Weight" class="item_weight form-control" id="item_weight"></div><div class="form-group col-md-6 col-xs-6" id="item_price'+i+'"><label class="control-label">Item price:</label><input type="number" name="item_price[]" placeholder="Item Price" class="item_price form-control" id="item_price"></div><a class="btn btn-add" style="color: #F69147;" id="add-item" title="Add new item" href="javascript: insertItem(\'dynamic_field'+i+'\')">Add Item</a> <span>|</span><a class="btn btn_remove" style="color: #F69147;" id='+i+' >Delete</a><br><br></div>').insertAfter('#'+elementId);
+		    $('<div style="border-top: 1px solid lightgray; margin-top: 25px" id="dynamic_field'+i+'" id="row'+i+'"><br><br><br><div class="form-group col-md-6 col-xs-6"  id="item_name'+i+'"><label class="control-label">Item Name:</label><input type="text" name="item_name[]" placeholder="Item Name" class="item_namae form-control" id="item_name"></div><div class="form-group col-md-6 col-xs-6"  id="item_quantity'+i+'"><label class="control-label">Item Quantity:</label><input type="number" name="item_quantity[]" placeholder="Item Quantity" class="item_quantity form-control" id="item_quantity"></div><div class="form-group col-md-6 col-xs-6" id="item_weight'+i+'"><label class="control-label">Item Weight(KG):</label><input type="number" name="item_weight[]" placeholder="Item Weight" class="item_weight form-control" id="item_weight"></div><div class="form-group col-md-6 col-xs-6" id="item_price'+i+'"><label class="control-label">Item price:</label><input type="number" name="item_price[]" placeholder="Item Price" class="item_price form-control" id="item_price"></div><a class="btn btn-add" style="color: #F69147;" id="add-item" title="Add new item" href="javascript: insertItem(\'dynamic_field'+i+'\')">Add Item</a> <span>|</span><a class="btn btn_remove" style="color: #F69147;" id='+i+' >Delete</a><br><br></div>').insertAfter('#'+elementId);
       }
       $(document).on('click', '.btn_remove', function() {
         var button_id = $(this).attr("id");
@@ -44,7 +46,6 @@
     </script>
 
   </head>
- 
 
   <body>
     <div class="navbar navbar-default" role="navigation">
@@ -61,7 +62,7 @@
 
           <a class="navbar-brand" href="#">
             <div class="logo">
-              <a href="http://new.saddleng.com"><img src="/saddleng/img/logo.png"/> </a>
+            <a href="http://new.saddleng.com"> <img src="/saddleng/img/logo.png"/> </a>
             </div>
           </a>
 
