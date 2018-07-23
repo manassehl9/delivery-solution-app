7b990d344231904f8125b9e11eacab2ca9445582
Manasseh Abiodun <manassehl9@gmail.com>
2018-07-23 10:02:25 +0100

    added online chat functionality

--- application/views/layouts/footer.php
+++ application/views/layouts/footer.php
@@ -4,5 +4,18 @@
     <script src="/saddleng/js/jquery.backstretch.min.js"></script>
     <script src="/saddleng/js/retina-1.1.0.min.js"></script>
     <script src="https://netpluspay.com/inlinepayment/js/inline.js"></script>
+    <!--Start of Tawk.to Script-->
+    <script type="text/javascript">
+      var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+      (function(){
+      var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+      s1.async=true;
+      s1.src='https://embed.tawk.to/5b4ef85291379020b95ef8c7/default';
+      s1.charset='UTF-8';
+      s1.setAttribute('crossorigin','*');
+      s0.parentNode.insertBefore(s1,s0);
+      })();
+    </script>
+    <!--End of Tawk.to Script-->
   </body>
 </html>
